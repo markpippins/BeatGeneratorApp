e7a5bd4ac66cf400739ed1e376ef63b7dec74dc5
markpippins <mpippins@gmail.com>
2023-03-03 15:29:55 -0500

    update

---
+++ java/src/main/java/com/angrysurfer/midi/service/TickerService.java
@@ -0,0 +1,2 @@
+package com.angrysurfer.midi.service;public class TickerService {
+}
---
+++ javascript/src/app/models/indexer.spec.ts
@@ -0,0 +1,7 @@
+import { Indexer } from './indexer';
+
+describe('Indexer', () => {
+  it('should create an instance', () => {
+    expect(new Indexer()).toBeTruthy();
+  });
+});
