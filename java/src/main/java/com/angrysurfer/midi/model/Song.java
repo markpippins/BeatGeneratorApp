79709c376bfad943eb19602397ce287ec22faeeb
markpippins <mpippins@gmail.com>
2023-03-11 04:59:48 -0500

    update

---
+++ java/src/main/java/com/angrysurfer/midi/model/Song.java
@@ -0,0 +1,17 @@
+package com.angrysurfer.midi.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.*;
+public class Song {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @OneToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "song_patterns", joinColumns = { @JoinColumn(name = "song_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "pattern_id") })
+	private List<Pattern> patterns = new ArrayList<>();
+}
