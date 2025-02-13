cfffbdfa9d75724c99fa6f7433920df2dfe8ef37
markpippins <mpippins@gmail.com>
2025-02-13 13:42:56 -0500

    got rid of visualizer names in Grid

--- java/swing/beatsui/src/main/java/com/angrysurfer/beatsui/surface/Grid.java
+++ java/swing/beatsui/src/main/java/com/angrysurfer/beatsui/surface/Grid.java
@@ -1,80 +1,18 @@
 package com.angrysurfer.beatsui.surface;
 
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
+import java.util.Set;
 
 import javax.swing.JComponent;
 import javax.swing.Timer;
 
+import org.reflections.Reflections;
+
 import com.angrysurfer.beatsui.api.StatusConsumer;
-import com.angrysurfer.beatsui.surface.visualization.ArpeggiatorVisualization;
-import com.angrysurfer.beatsui.surface.visualization.AsteroidsVisualization;
-import com.angrysurfer.beatsui.surface.visualization.BinaryRainVisualization;
-import com.angrysurfer.beatsui.surface.visualization.BounceVisualization;
-import com.angrysurfer.beatsui.surface.visualization.BreakoutVisualization;
-import com.angrysurfer.beatsui.surface.visualization.BrownianVisualization;
-import com.angrysurfer.beatsui.surface.visualization.CellularVisualization;
-import com.angrysurfer.beatsui.surface.visualization.ChordProgressionVisualization;
-import com.angrysurfer.beatsui.surface.visualization.ClockVisualization;
-import com.angrysurfer.beatsui.surface.visualization.ConfettiVisualization;
-import com.angrysurfer.beatsui.surface.visualization.CrystalVisualization;
-import com.angrysurfer.beatsui.surface.visualization.DNAVisualization;
-import com.angrysurfer.beatsui.surface.visualization.DigDugVisualization;
-import com.angrysurfer.beatsui.surface.visualization.DrumPatternVisualization;
-import com.angrysurfer.beatsui.surface.visualization.EqualizerVisualization;
-import com.angrysurfer.beatsui.surface.visualization.EuclideanRhythmVisualization;
-import com.angrysurfer.beatsui.surface.visualization.ExplosionVisualization;
-import com.angrysurfer.beatsui.surface.visualization.FireworksVisualization;
-import com.angrysurfer.beatsui.surface.visualization.FrequencyBandsVisualization;
-import com.angrysurfer.beatsui.surface.visualization.FroggerVisualization;
-import com.angrysurfer.beatsui.surface.visualization.GameOfLifeVisualization;
-import com.angrysurfer.beatsui.surface.visualization.GateSequencerVisualization;
-import com.angrysurfer.beatsui.surface.visualization.HarmonicsVisualization;
-import com.angrysurfer.beatsui.surface.visualization.HeartVisualization;
-import com.angrysurfer.beatsui.surface.visualization.KaleidoscopeVisualization;
-import com.angrysurfer.beatsui.surface.visualization.LFOMatrixVisualization;
-import com.angrysurfer.beatsui.surface.visualization.LangtonVisualization;
-import com.angrysurfer.beatsui.surface.visualization.LifeSoupVisualization;
-import com.angrysurfer.beatsui.surface.visualization.LoopPulseVisualization;
-import com.angrysurfer.beatsui.surface.visualization.MandelbrotVisualization;
-import com.angrysurfer.beatsui.surface.visualization.MatrixRainVisualization;
-import com.angrysurfer.beatsui.surface.visualization.MatrixVisualization;
-import com.angrysurfer.beatsui.surface.visualization.MazeVisualization;
-import com.angrysurfer.beatsui.surface.visualization.MidiGridVisualization;
-import com.angrysurfer.beatsui.surface.visualization.MissileCommandVisualization;
-import com.angrysurfer.beatsui.surface.visualization.ModularCVVisualization;
-import com.angrysurfer.beatsui.surface.visualization.OscilloscopeVisualization;
-import com.angrysurfer.beatsui.surface.visualization.PacmanVisualization;
-import com.angrysurfer.beatsui.surface.visualization.PhaseShiftVisualization;
-import com.angrysurfer.beatsui.surface.visualization.PianoRollVisualization;
-import com.angrysurfer.beatsui.surface.visualization.PlasmaVisualization;
-import com.angrysurfer.beatsui.surface.visualization.PlatformClimberVisualization;
-import com.angrysurfer.beatsui.surface.visualization.PolePositionVisualization;
-import com.angrysurfer.beatsui.surface.visualization.PolyphonicVisualization;
-import com.angrysurfer.beatsui.surface.visualization.PolyrhythmVisualization;
-import com.angrysurfer.beatsui.surface.visualization.PongVisualization;
-import com.angrysurfer.beatsui.surface.visualization.ProbabilityGridVisualization;
-import com.angrysurfer.beatsui.surface.visualization.PulseVisualization;
-import com.angrysurfer.beatsui.surface.visualization.RacingVisualization;
-import com.angrysurfer.beatsui.surface.visualization.RainbowJapaneseMatrixVisualization;
-import com.angrysurfer.beatsui.surface.visualization.RainbowVisualization;
-import com.angrysurfer.beatsui.surface.visualization.RippleVisualization;
-import com.angrysurfer.beatsui.surface.visualization.RubiksCompVisualization;
-import com.angrysurfer.beatsui.surface.visualization.SnakeVisualization;
-import com.angrysurfer.beatsui.surface.visualization.SpaceInvadersVisualization;
-import com.angrysurfer.beatsui.surface.visualization.SpaceVisualization;
-import com.angrysurfer.beatsui.surface.visualization.SpectrumAnalyzerVisualization;
-import com.angrysurfer.beatsui.surface.visualization.SpiralVisualization;
-import com.angrysurfer.beatsui.surface.visualization.StarfieldVisualization;
-import com.angrysurfer.beatsui.surface.visualization.StepSequencerVisualization;
-import com.angrysurfer.beatsui.surface.visualization.TimeDivisionVisualization;
-import com.angrysurfer.beatsui.surface.visualization.TriggerBurstVisualization;
-import com.angrysurfer.beatsui.surface.visualization.TronVisualization;
-import com.angrysurfer.beatsui.surface.visualization.VUMeterVisualization;
-import com.angrysurfer.beatsui.surface.visualization.WaveVisualization;
-import com.angrysurfer.beatsui.surface.visualization.XYPadVisualization;
 import com.angrysurfer.beatsui.widget.GridButton;
 
 import lombok.Getter;
@@ -115,73 +53,29 @@ public class Grid {
     }
 
     private void initializeVisualizations() {
-        visualizations.put(VisualizationEnum.EXPLOSION, new ExplosionVisualization());
-        visualizations.put(VisualizationEnum.SPACE, new SpaceVisualization());
-        visualizations.put(VisualizationEnum.GAME, new GameOfLifeVisualization());
-        visualizations.put(VisualizationEnum.RAIN, new MatrixRainVisualization());
-        visualizations.put(VisualizationEnum.WAVE, new WaveVisualization());
-        visualizations.put(VisualizationEnum.BOUNCE, new BounceVisualization());
-        visualizations.put(VisualizationEnum.SNAKE, new SnakeVisualization());
-        visualizations.put(VisualizationEnum.SPIRAL, new SpiralVisualization());
-        visualizations.put(VisualizationEnum.FIREWORKS, new FireworksVisualization());
-        visualizations.put(VisualizationEnum.CONFETTI, new ConfettiVisualization());
-        visualizations.put(VisualizationEnum.MATRIX, new MatrixVisualization());
-        visualizations.put(VisualizationEnum.HEART, new HeartVisualization());
-        visualizations.put(VisualizationEnum.DNA, new DNAVisualization());
-        visualizations.put(VisualizationEnum.SPECTRUM_ANALYZER, new SpectrumAnalyzerVisualization());
-        visualizations.put(VisualizationEnum.VU_METERS, new VUMeterVisualization());
-        visualizations.put(VisualizationEnum.PLASMA, new PlasmaVisualization());
-        visualizations.put(VisualizationEnum.MANDELBROT, new MandelbrotVisualization());
-        visualizations.put(VisualizationEnum.KALEIDOSCOPE, new KaleidoscopeVisualization());
-        visualizations.put(VisualizationEnum.RIPPLE, new RippleVisualization());
-        visualizations.put(VisualizationEnum.PACMAN, new PacmanVisualization());
-        visualizations.put(VisualizationEnum.INVADERS, new SpaceInvadersVisualization());
-        visualizations.put(VisualizationEnum.STEP_SEQUENCER, new StepSequencerVisualization());
-        visualizations.put(VisualizationEnum.DRUM_PATTERN, new DrumPatternVisualization());
-        visualizations.put(VisualizationEnum.EQUALIZER, new EqualizerVisualization());
-        visualizations.put(VisualizationEnum.OSCILLOSCOPE, new OscilloscopeVisualization());
-        visualizations.put(VisualizationEnum.FREQUENCY_BANDS, new FrequencyBandsVisualization());
-        visualizations.put(VisualizationEnum.MIDI_GRID, new MidiGridVisualization());
-        visualizations.put(VisualizationEnum.PIANO_ROLL, new PianoRollVisualization());
-        visualizations.put(VisualizationEnum.ARPEGGIATOR, new ArpeggiatorVisualization());
-        visualizations.put(VisualizationEnum.POLYPHONIC, new PolyphonicVisualization());
-        visualizations.put(VisualizationEnum.MODULAR, new ModularCVVisualization());
-        visualizations.put(VisualizationEnum.LFO_MATRIX, new LFOMatrixVisualization());
-        visualizations.put(VisualizationEnum.PHASE_SHIFT, new PhaseShiftVisualization());
-        visualizations.put(VisualizationEnum.GATE_SEQ, new GateSequencerVisualization());
-        visualizations.put(VisualizationEnum.CHORD_PROG, new ChordProgressionVisualization());
-        visualizations.put(VisualizationEnum.HARMONICS, new HarmonicsVisualization());
-        visualizations.put(VisualizationEnum.TIME_DIVISION, new TimeDivisionVisualization());
-        visualizations.put(VisualizationEnum.POLYRHYTHM, new PolyrhythmVisualization());
-        visualizations.put(VisualizationEnum.XY_PAD, new XYPadVisualization());
-        visualizations.put(VisualizationEnum.LOOP_PULSE, new LoopPulseVisualization());
-        visualizations.put(VisualizationEnum.TRIG_BURST, new TriggerBurstVisualization());
-        visualizations.put(VisualizationEnum.EUCLID, new EuclideanRhythmVisualization());
-        visualizations.put(VisualizationEnum.PROBABILITY, new ProbabilityGridVisualization());
-        visualizations.put(VisualizationEnum.PULSE, new PulseVisualization());
-        visualizations.put(VisualizationEnum.RAINBOW, new RainbowVisualization());
-        visualizations.put(VisualizationEnum.PONG, new PongVisualization());
-        visualizations.put(VisualizationEnum.BREAKOUT, new BreakoutVisualization());
-        visualizations.put(VisualizationEnum.ASTEROID, new AsteroidsVisualization());
-        visualizations.put(VisualizationEnum.MISSILE, new MissileCommandVisualization());
-        visualizations.put(VisualizationEnum.TRON, new TronVisualization());
-        visualizations.put(VisualizationEnum.DIGDUG, new DigDugVisualization());
-        visualizations.put(VisualizationEnum.CLIMBER, new PlatformClimberVisualization());
-        visualizations.put(VisualizationEnum.RACING, new RacingVisualization());
-        visualizations.put(VisualizationEnum.CLOCK, new ClockVisualization());
-        visualizations.put(VisualizationEnum.STARFIELD, new StarfieldVisualization());
-        visualizations.put(VisualizationEnum.MAZE, new MazeVisualization());
-        visualizations.put(VisualizationEnum.CRYSTAL, new CrystalVisualization());
-        visualizations.put(VisualizationEnum.CELLULAR, new CellularVisualization());
-        visualizations.put(VisualizationEnum.LIFE_SOUP, new LifeSoupVisualization());
-        visualizations.put(VisualizationEnum.BROWNIAN, new BrownianVisualization());
-        visualizations.put(VisualizationEnum.BINARY, new BinaryRainVisualization());
-        visualizations.put(VisualizationEnum.LANGTON, new LangtonVisualization());
-        visualizations.put(VisualizationEnum.FROGGER, new FroggerVisualization());
-        visualizations.put(VisualizationEnum.POLE_POSITION, new PolePositionVisualization());
-        visualizations.put(VisualizationEnum.RUBIKS_COMP, new RubiksCompVisualization());
-        visualizations.put(VisualizationEnum.RAINBOW_MATRIX, new RainbowJapaneseMatrixVisualization());
-        // Add more visualizations as we create them...
+        visualizations = new HashMap<>();
+        try {
+            Reflections reflections = new Reflections("com.angrysurfer.beatsui.surface.visualization");
+            Set<Class<? extends VisualizationHandler>> visualizationClasses = 
+                reflections.getSubTypesOf(VisualizationHandler.class);
+
+            for (Class<? extends VisualizationHandler> vizClass : visualizationClasses) {
+                // Skip abstract classes and interfaces
+                if (!Modifier.isAbstract(vizClass.getModifiers()) && !vizClass.isInterface()) {
+                    try {
+                        VisualizationHandler handler = vizClass.getDeclaredConstructor().newInstance();
+                        VisualizationEnum vizEnum = handler.getEnum();
+                        if (vizEnum != null) {
+                            visualizations.put(vizEnum, handler);
+                        }
+                    } catch (Exception e) {
+                        System.err.println("Failed to instantiate " + vizClass.getSimpleName() + ": " + e.getMessage());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Error initializing visualizations: " + e.getMessage());
+        }
     }
 
     private void setupTimers() {
