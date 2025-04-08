11106d5ed5625821b90bd8179b7ccafc795083ab
Mark Pippins <mpippins@gmail.com>
2025-04-08 00:57:45 -0400

    drum sequencer nav bar works

---
+++ java/core/src/main/java/com/angrysurfer/core/sequencer/MelodicSequenceData.java
@@ -0,0 +1,42 @@
+package com.angrysurfer.core.sequencer;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.angrysurfer.core.model.Direction;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Data transfer object for serializing melodic sequence data
+ */
+@Getter
+@Setter
+public class MelodicSequenceData implements Serializable {
+    private Long id;
+    private Integer sequencerId;
+    
+    // Sequence parameters
+    private int patternLength;
+    private Direction direction;
+    private TimingDivision timingDivision;
+    private boolean looping;
+    private int octaveShift;
+    
+    // Quantization parameters
+    private boolean quantizeEnabled;
+    private String rootNote;
+    private String scale;
+    
+    // Pattern data
+    private List<Boolean> activeSteps;
+    private List<Integer> noteValues;
+    private List<Integer> velocityValues;
+    private List<Integer> gateValues;
+    
+    // Constructor
+    public MelodicSequenceData() {
+        // Default constructor
+    }
+}
